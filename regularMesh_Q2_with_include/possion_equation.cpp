991b9f0cea635cb52ccc50fc77231ad403ed47fd
LiShiJie2267 <lsj1018845759@outlook.com>
2020-06-28 14:01:24 +0800

    一些修改

--- regularMesh_Q2_with_include/possion_equation.cpp
+++ regularMesh_Q2_with_include/possion_equation.cpp
@@ -5,23 +5,6 @@
  * 
  * @brief  利用AFEPack里的函数以及功能，划分结构化网格，使用lagrange九节点二次基函数
  * 		　　，实现Possion方程编值问题计算．
- * 			说明：
- * 			1、利用std::pair<double,double>每一个节点的坐标；
- * 			   一维问题可以直接代替为double，三维问题目前初步考虑用std::vector<double>代替;
- * 			2、利用std::pair<std::pair<double,double>,unsigned int> 存储每一个节点坐标和局部编号；
- * 			3、利用std::map<std::pair<std::pair<double,double>,unsigned int>,unsigned int> 
- * 			   建立局部信息和整体编号之间的对应关系；
- * 			4、利用std::vector<std::map<std::pair<std::pair<double,double>,unsigned int>,unsigned int> >
- * 			   存储所有单元局部信息和整体编号之前的对应关系等信息；
- * 			5、定义宏函数local_coord_index_gen(n,x0,x1,y0,y1,i,j,local_index)，生成局部信息和整体编号之前的对应关系，
- * 			   其中n为网划分段数(可扩展为n1、n2、n3表示三维问题),x0 ~ y1为计算区域，i,j为单元里的位置参数；
- * 			   local_index为单元里局部编号；
- * 			6、定义宏函数global_to_coord(global_index,n,h)，计算整体编号和节点坐标之间的关系，其中global_index为整体编号；
- * 			7、定义宏函数可以减少结构化代码重复次数，同时可以提高程序的复用性和可读性；
- * 			8、定义宏函数之后Q1和Q2的实现方式可以趋于一致化，唯一不同的就是148-163行；
- *  		9、最后手动计算L2误差，by王老师。
- * 
- * 
  */
 #include <iostream>
 #include <cmath>
@@ -99,6 +82,7 @@ int main(int argc, char* argv[])
 	domain.set_divide_mode("Q2");
 	domain.initial_2D_rectangle_domain(x0,x1,y0,y1,nx,ny);
 	domain.generate_mesh();
+	domain.generate_boundary();
 	Vector<double> rhs(dim);
 	std::vector<unsigned int> boundary(dim);
 	boundary[0]=boundary[2 * n]=boundary[2 * n * (2 * n + 1)]=boundary[dim - 1] = 1;
@@ -160,40 +144,36 @@ int main(int argc, char* argv[])
 		}	
 	}
 	
-	for(int j=0;j<=2*n;j++)
-	for(int i=0;i<=2*n;i++)
+	for(auto& index:domain.boundary)
 	{
-		int index = j * ( 2 * n + 1 ) + i;
-		if(boundary[index]==1)
-		{	
-			double x =((2 * n - i )* x0 + i * x1)/( 2 * n);
-			double y =((2 * n - j )* y0 + j * y1)/( 2 * n);
-			SparseMatrix<double>::iterator row_iterator = stiff_mat.begin(index);
-    	    SparseMatrix<double>::iterator row_end = stiff_mat.end(index);
-    	    double diag = row_iterator->value();
-			AFEPack::Point<2> bnd_point;
-			bnd_point[0]=x;
-			bnd_point[1]=y;
-    	    double bnd_value = u(bnd_point);
-            rhs(index) = diag * bnd_value;
-    	    for ( ++row_iterator; row_iterator != row_end; ++row_iterator)
-            {
-            	row_iterator->value() = 0.0;
-    			int k = row_iterator->column();
-                SparseMatrix<double>::iterator col_iterator = stiff_mat.begin(k);   
-                SparseMatrix<double>::iterator col_end = stiff_mat.end(k);
-    	    	for (++col_iterator; col_iterator != col_end; ++col_iterator)
-    				if (col_iterator->column() == index)
-    			    	break;
-    			if (col_iterator == col_end)
-    			{
-    				std::cerr << "Error!" << std::endl;
-    				exit(-1);
-    			}
-    			rhs(k) -= col_iterator->value() * bnd_value;
-    			col_iterator->value() = 0.0;	
-            }  
-		}
+		
+		double x = domain.get_point_coord(index,0);
+		double y = domain.get_point_coord(index,1);
+		SparseMatrix<double>::iterator row_iterator = stiff_mat.begin(index);
+    	SparseMatrix<double>::iterator row_end = stiff_mat.end(index);
+    	double diag = row_iterator->value();
+		AFEPack::Point<2> bnd_point;
+		bnd_point[0]=x;
+		bnd_point[1]=y;
+    	double bnd_value = u(bnd_point);
+        rhs(index) = diag * bnd_value;
+    	for ( ++row_iterator; row_iterator != row_end; ++row_iterator)
+        {
+            row_iterator->value() = 0.0;
+    		int k = row_iterator->column();
+            SparseMatrix<double>::iterator col_iterator = stiff_mat.begin(k);   
+            SparseMatrix<double>::iterator col_end = stiff_mat.end(k);
+    	    for (++col_iterator; col_iterator != col_end; ++col_iterator)
+    			if (col_iterator->column() == index)
+    			    break;
+    		if (col_iterator == col_end)
+    		{
+    			std::cerr << "Error!" << std::endl;
+    			exit(-1);
+    		}
+    		rhs(k) -= col_iterator->value() * bnd_value;
+    		col_iterator->value() = 0.0;	
+        } 
 	}
 
 	AMGSolver solver(stiff_mat);
