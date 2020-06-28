27d9619172a2a4d463d81d5f2332eb6129bb7a0f
LiShiJie2267 <lsj1018845759@outlook.com>
2020-06-28 13:52:31 +0800

    一些修改

--- regularMesh_Q1_with_include/possion_equation.cpp
+++ regularMesh_Q1_with_include/possion_equation.cpp
@@ -7,31 +7,6 @@
  * 
  * @copyright Copyright (c) 2020
  */
-/**
- * @file   possion_equation.cpp
- * @author Lishijie (lsj1018845759@outlook.com)
- * @brief  利用AFEPack里的函数以及功能，划分结构化网格，使用双线性四节点基函数
- * 		　　，实现Possion方程编值问题计算．
- * 			说明：
- * 			1、利用std::pair<double,double>每一个节点的坐标；
- * 			   一维问题可以直接代替为double，三维问题目前初步考虑用std::vector<double>代替;
- * 			2、利用std::pair<std::pair<double,double>,unsigned int> 存储每一个节点坐标和局部编号；
- * 			3、利用std::map<std::pair<std::pair<double,double>,unsigned int>,unsigned int> 
- * 			   建立局部信息和整体编号之间的对应关系；
- * 			4、利用std::vector<std::map<std::pair<std::pair<double,double>,unsigned int>,unsigned int> >
- * 			   存储所有单元局部信息和整体编号之前的对应关系等信息；
- * 			5、定义宏函数local_coord_index_gen(n,x0,x1,y0,y1,i,j,local_index)，生成局部信息和整体编号之前的对应关系，
- * 			   其中n为网划分段数(可扩展为n1、n2、n3表示三维问题),x0 ~ y1为计算区域，i,j为单元里的位置参数；
- * 			   local_index为单元里局部编号；
- * 			6、定义宏函数global_to_coord(global_index,n,h)，计算整体编号和节点坐标之间的关系，其中global_index为整体编号；
- * 			7、定义宏函数可以减少结构化代码重复次数，同时可以提高程序的复用性和可读性；
- *			8、最后手动计算L2误差，by王老师。
- * @version 0.1
- * @date   2020-06-13
- * 
- * @copyright Copyright (c) 2020
- */		
-
 #include <iostream>
 #include <cmath>
 #include <utility>
@@ -97,8 +72,8 @@ int main(int argc, char* argv[])
     double y0 = 0.0;
     double x1 = 1.0;
     double y1 = 1.0;
-	int nx=50;
-	int ny=40;
+	int nx=20;
+	int ny=20;
 	//利用include里的函数RectangleDomain<2>对网格参数进行初始化；
 	RectangleDomain<2> domain;
 	/// 利用网格自动生成函数；
@@ -106,6 +81,7 @@ int main(int argc, char* argv[])
 	/// 生成网格，可以用domain.Mesh进行操作；由于处于测试版本，Mesh为public可改进；
 	/// 这里网格生成的默认格式为四边形(二维情况)或六面体(三维情况)；可改进；
 	domain.generate_mesh();
+	domain.generate_boundary();
 	/// 设置剖分断数和节点总数
 	int dim = ( domain.get_Divide( 0 ) + 1 ) * ( domain.get_Divide( 1 ) + 1 );
 	Vector<double> rhs( dim );
@@ -157,30 +133,27 @@ int main(int argc, char* argv[])
 		}	
 	}
 	///处理所有边界条件
-	for(unsigned int index=0;index < dim;index++)
+	for(auto& index:domain.boundary)
 	{
-		if(nozeroperow[index]==4||nozeroperow[index]==6)
-		{	
-			double x=domain.get_point_coord(index,0);
-			double y=domain.get_point_coord(index,1);
-			//std::cout<<"("<<x<<","<<y<<")"<<std::endl;
-			SparseMatrix<double>::iterator row_iterator = stiff_mat.begin(index);
-    	    SparseMatrix<double>::iterator row_end = stiff_mat.end(index);
-    	    double diag = row_iterator->value();
-			AFEPack::Point<2> bnd_point;
-			bnd_point[0]=x;
-			bnd_point[1]=y;
-    	    double bnd_value = u(bnd_point);
-            rhs(index) = diag * bnd_value;
-    	    for ( ++row_iterator; row_iterator != row_end; ++row_iterator)
-            {
-            	row_iterator->value() = 0.0;
-    			int k = row_iterator->column();
-                SparseMatrix<double>::iterator col_iterator = stiff_mat.begin(k);   
-                SparseMatrix<double>::iterator col_end = stiff_mat.end(k);
-    	    	for (++col_iterator; col_iterator != col_end; ++col_iterator)
-    				if (col_iterator->column() == index)
-    			    	break;
+		double x=domain.get_point_coord(index,0);
+		double y=domain.get_point_coord(index,1);
+		SparseMatrix<double>::iterator row_iterator = stiff_mat.begin(index);
+    	SparseMatrix<double>::iterator row_end = stiff_mat.end(index);
+    	double diag = row_iterator->value();
+		AFEPack::Point<2> bnd_point;
+		bnd_point[0]=x;
+		bnd_point[1]=y;
+    	double bnd_value = u(bnd_point);
+        rhs(index) = diag * bnd_value;
+    	for ( ++row_iterator; row_iterator != row_end; ++row_iterator)
+        {
+            row_iterator->value() = 0.0;
+    		int k = row_iterator->column();
+            SparseMatrix<double>::iterator col_iterator = stiff_mat.begin(k);   
+            SparseMatrix<double>::iterator col_end = stiff_mat.end(k);
+    	    for (++col_iterator; col_iterator != col_end; ++col_iterator)
+    			if (col_iterator->column() == index)
+    			    break;
     			if (col_iterator == col_end)
     			{
     				std::cerr << "Error!" << std::endl;
@@ -188,8 +161,8 @@ int main(int argc, char* argv[])
     			}
     			rhs(k) -= col_iterator->value() * bnd_value;
     			col_iterator->value() = 0.0;	
-            }  
-		}
+        }  
+		
 	}
 	///　用代数多重网格(AMG)计算线性方程
 	AMGSolver solver(stiff_mat);
@@ -219,10 +192,6 @@ int main(int argc, char* argv[])
 	double error = 0.0;
     for(unsigned int index = 0; index < dim; index++)
     {
-	//int x_num = index % (n + 1);
-	//int y_num = index / (n + 1);
-	//double x = x_num * h;	
-	//double y = y_num * h;
 	double x=domain.get_point_coord(index,0);
 	double y=domain.get_point_coord(index,1);
 	AFEPack::Point<2> pnt;
